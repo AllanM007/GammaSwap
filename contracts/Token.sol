221e8705772c646f34eaf1ca6b686988c2d3007f
mzawadi <mwaranguallan345@gmail.com>
2022-01-19 11:32:38 +0300

    update Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,11 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GammaToken is ERC2O {
+contract GammaToken is ERC20 {
     constructor(uint256 totalSupply) ERC20("GammaToken", "GM"){
         _mint(msg.sender, totalSupply);
     }
