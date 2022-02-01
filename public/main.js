985397185ce500e99d0c371a5b4ef98e978b8a1f
mzawadi <mwaranguallan345@gmail.com>
2022-02-01 17:01:14 +0300

    final update feb 1 & transfer key to env

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
 console.log("Tebere");
 
 const Web3 = require('web3');
-const rpcURL = require('https://polygon-mumbai.g.alchemy.com/v2/H8Djcrme5eDEstvMB_ZVzOrT12RbUj0d');
-const web3URL = new Web3(rpcURL);
-  const account = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
-const GammaTokenAbi = require('./artifacts/contracts/Token.sol/GammaToken.json');
+const rpcURL = 'https://mainnet.infura.io/v3/fae05634594f42eb846fd4023d367efd'
+const web3 = new Web3(rpcURL);
+const address = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
 
-const address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
-const contract = new web3.eth.Contract(GammaTokenAbi, address);
+const GammaTokenAbi = require('../artifacts/contracts/Token.sol/GammaToken.json');
+const contract_address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
+const contract = new web3.eth.Contract(GammaTokenAbi, contract_address);
 
 if (typeof window.ethereum !== 'undefined') {
   console.log('MetaMask is installed!');
@@ -24,12 +24,10 @@ async function getAccount() {
   document.querySelector('#showAccount').innerHTML = account;
 }
 
-const web3 = new Web3(rpcURL);
-const account = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
-
 web3.eth.getBalance(address, (err, wei) => {
   balance = web3.utils.fromWei(wei, 'ether');
   console.log(balance);
 })
 
 console.log(contract.methods);
+console.log("RUNNING MAN!!!");
