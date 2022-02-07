47e6431e4964397e337adced2312244b9f118696
mzawadi <mwaranguallan345@gmail.com>
2022-02-07 14:53:10 +0300

    add polyfill resolve fix for webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
     aggregateTimeout: 300, // After seeing an edit, wait .3 seconds to recompile
     poll: 500 // Check for edits every 5 seconds
   },
+  resolve: {
+    fallback: {
+        assert: require.resolve('assert'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        stream: require.resolve('stream-browserify'),
+    },
+  },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
     new webpack.ProgressPlugin(),
