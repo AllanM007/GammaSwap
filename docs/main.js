1620ef470ac450d6d199d8be8b4ff8f9d0960b29
mzawadi <mwaranguallan345@gmail.com>
2022-02-08 11:34:45 +0300

    update docs folder main.js/index.html

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,16 @@
-console.log("Tebere");
+// console.log("Tebere");
 
 const Web3 = require('web3');
-const rpcURL = require('https://polygon-mumbai.g.alchemy.com/v2/H8Djcrme5eDEstvMB_ZVzOrT12RbUj0d');
-const web3URL = new Web3(rpcURL);
-  const account = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
-const GammaTokenAbi = require('./artifacts/contracts/Token.sol/GammaToken.json');
-
-const address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
-const contract = new web3.eth.Contract(GammaTokenAbi, address);
-
-if (typeof window.ethereum !== 'undefined') {
-  console.log('MetaMask is installed!');
-}
-
-function EthButtonClick() {
-  getAccount();
-}
-
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  console.log(account);
-  document.querySelector('#showAccount').innerHTML = account;
-}
-
+// const rpcURL = 'https://mainnet.infura.io/v3/'[`${process.env.INFURA_KEY}`]
+const rpcURL = 'https://mainnet.infura.io/v3/fae05634594f42eb846fd4023d367efd'
 const web3 = new Web3(rpcURL);
-const account = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
+const address = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
+
+const GammaTokenAbi = require('../artifacts/contracts/Token.sol/GammaToken.json');
+const contract_address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
+const contract = new web3.eth.Contract(GammaTokenAbi, contract_address);
+
+console.log(rpcURL);
 
 web3.eth.getBalance(address, (err, wei) => {
   balance = web3.utils.fromWei(wei, 'ether');
@@ -33,3 +18,4 @@ web3.eth.getBalance(address, (err, wei) => {
 })
 
 console.log(contract.methods);
+console.log("RUNNING MAN!!!");
--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
-console.log("Tebere");
+// console.log("Tebere");
 
 const Web3 = require('web3');
+// const rpcURL = 'https://mainnet.infura.io/v3/'[`${process.env.INFURA_KEY}`]
 const rpcURL = 'https://mainnet.infura.io/v3/fae05634594f42eb846fd4023d367efd'
 const web3 = new Web3(rpcURL);
 const address = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
@@ -9,20 +10,7 @@ const GammaTokenAbi = require('../artifacts/contracts/Token.sol/GammaToken.json'
 const contract_address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
 const contract = new web3.eth.Contract(GammaTokenAbi, contract_address);
 
-if (typeof window.ethereum !== 'undefined') {
-  console.log('MetaMask is installed!');
-}
-
-function EthButtonClick() {
-  getAccount();
-}
-
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  console.log(account);
-  document.querySelector('#showAccount').innerHTML = account;
-}
+console.log(rpcURL);
 
 web3.eth.getBalance(address, (err, wei) => {
   balance = web3.utils.fromWei(wei, 'ether');
