786f11821071c1a0e6f2bb04c6bbece410d67635
mzawadi <mwaranguallan345@gmail.com>
2022-02-13 00:54:14 +0300

    update main.js file render index html page

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,6 @@
 // Load http
 const http = require("http");
+const fs = require("fs");
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -7,16 +8,17 @@ require('dotenv').config()
 
 // Load web3
 const Tx = require('ethereumjs-tx').Transaction;
-console.log(Tx);
 const Web3 = require('web3');
 const rpcURL = 'https://ropsten.infura.io/v3/' + `${process.env.INFURA_KEY}`
 const web3 = new Web3(rpcURL);
 const address = '0x2A20380DcA5bC24D052acfbf79ba23e988ad0050';
 
+// load contract parameters
 const abi = require('../artifacts/contracts/Token.sol/GammaToken.json');
 const contract_address = "0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12";
 const contract = new web3.eth.Contract(abi, contract_address);
 
+//load test account details
 const account1 = "0x38134439548De25F835116cA7d76C443D262f8fa";
 const account2 = "0x0D5Fa1C2967904fEB4A7A626bd83E4eF898824EB";
 
@@ -29,7 +31,7 @@ const server = http.createServer((req, res) => {
   // get address balance
   web3.eth.getBalance(address, (err, wei) => {
     balance = web3.utils.fromWei(wei, 'ether');
-    // console.log(wei);
+    // console.log(balance);
   })
   
   //log functions in contract
@@ -57,19 +59,25 @@ const server = http.createServer((req, res) => {
     
     const serializedTx = tx.serialize()
     const raw = '0x' + serializedTx.toString('hex')
-    // console.log(raw);
     
     //broadcast the transaction to the chain
-    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-      // console.log(err);
-      console.log('txHash:', txHash)
-    })
+    // web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    //   console.log('txHash:', txHash)
+    // })
   });
 
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/plain');
-  res.end('./docs/index.html');
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./docs/index.html', null, function (error, data) {
+    if (error) {
+        res.writeHead(404);
+        res.write('Whoops! File not found!');
+    } else {
+        res.write(data);
+    }
+    res.end();
+});
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
