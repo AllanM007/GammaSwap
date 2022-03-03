4d84e7bd0da83cc86835aa3c34cf46f7be7dff13
mzawadi <mwaranguallan345@gmail.com>
2022-03-03 12:31:57 +0300

    update Pool contract

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -32,7 +32,7 @@ contract PoolBank {
 
     // in constructor pass in the address for USDC token and your custom token
     // that will be used to pay interest
-    constructor() public {
+    constructor() {
         usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         // GammaToken = 0x3e7b1fdf5d6ef11ec168df92df6c745fb8d7fb12;
         GammaToken = 0x3E7B1fDF5d6ef11ec168df92df6C745FB8D7FB12;
