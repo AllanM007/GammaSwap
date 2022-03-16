c85ded4407d4332af259eff08a224b662ad2bd31
mzawadi <mwaranguallan345@gmail.com>
2022-03-16 16:21:37 +0300

    add gas limit parameter to contract function

--- scripts/interact.js
+++ scripts/interact.js
@@ -2,6 +2,7 @@ const API_KEY = process.env.ALCHEMY_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.GammaSwapTrade_ADDRESS;
 
+const { json } = require("hardhat/internal/core/params/argumentTypes");
 const contract = require("../artifacts/contracts/Trade.sol/TokenSwap.json");
 
 // Provider
@@ -15,7 +16,7 @@ const gammaSwapContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, si
 
 async function main() {
 
-    const message = await gammaSwapContract.buy(1);
+    const message = await gammaSwapContract.buy(1, { gasLimit: 250000 });
     console.log("The message is: " + message);
 }
 main();
