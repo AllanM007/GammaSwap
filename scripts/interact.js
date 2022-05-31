104985102a47394760f6ceb0bd9b1d53f067e022
mzawadi <mwaranguallan345@gmail.com>
2022-06-01 00:38:34 +0300

    update interact.js script

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const API_KEY = process.env.ALCHEMY_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.GammaSwapTrade_ADDRESS;
 
 const { json } = require("hardhat/internal/core/params/argumentTypes");
+const { ethers } = require("ethers");
 const contract = require("../artifacts/contracts/Trade.sol/TokenSwap.json");
 
 // Provider
@@ -11,12 +13,14 @@ const alchemyProvider = new ethers.providers.AlchemyProvider(network="ropsten", 
 // Signer
 const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 
+console.log(signer.getGasPrice());
+
 // Contract
 const gammaSwapContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
 
-    const message = await gammaSwapContract.transfer("0x391E3567e8Da8018f592e1855A4459629c0E1d8A", 50, { gasLimit: 250000 });
+    const message = await gammaSwapContract.buy(50, { gasLimit: 250000 });
     console.log("The message is: " + message);
 }
 main();
