4db49b1b7ccdfad003be3cd737fb8cfaeb2e33c4
mzawadi <mwaranguallan345@gmail.com>
2022-06-21 14:12:42 +0300

    final js buy function

--- scripts/interact.js
+++ scripts/interact.js
@@ -21,6 +21,30 @@ console.log(signer.getGasPrice());
 const gammaSwapContract = new ethers.Contract("0x86D00C262ed816b329ADC799bB1EFF01E38d5324", contract.abi, signer);
 const gammaTokenContract = new ethers.Contract(process.env.GammaToken_ADDRESS, GammaTokenContractABI.abi, signer );
 
+async function buyToken() {
+    
+    const recipientAddr = '0x391E3567e8Da8018f592e1855A4459629c0E1d8A';
+    
+    const buyNum = 49899999;
+    const fmtInt = ethers.utils.parseUnits(buyNum.toString(), 18);
+
+    try {
+        
+        const buyTransaction = await gammaSwapContract.buy(20, { gasLimit: 250000 });
+        const buyLog = await buyTransaction.wait();
+
+        const buyObject = buyLog.events.find(event => event.event === 'Bought');
+
+        const [to, value] = buyObject.args;
+            
+        console.log(to, value.toString());
+              
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 async function sellToken() {
     
     const recipientAddr = '0x391E3567e8Da8018f592e1855A4459629c0E1d8A';
@@ -53,4 +77,5 @@ async function sellToken() {
     }
 }
 
-sellToken();
+buyToken();
+// sellToken();
